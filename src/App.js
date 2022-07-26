5b96e72d90d605abca688cc00588858e31991c93
Leaf of July <work2357@icloud.com>
2022-07-26 16:53:40 +0900

    feat: Create blog project with REACT

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const post = '강남 우동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color: 'red', fontSize: '16px'}}>블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
